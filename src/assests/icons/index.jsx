f65117431001fbbcb058cc492ff02a25b2075d97
786TOUSEEF <71210711+786TOUSEEF@users.noreply.github.com>
2021-04-15 00:35:25 +0500

    Forms Module

--- src/assests/icons/index.jsx
+++ src/assests/icons/index.jsx
@@ -1,3 +1,6 @@
-export { Person,Dashboard,History,Today,AttachMoney,Group,People} from '@material-ui/icons';
+export { 
+    Person,Dashboard,History,Today,AttachMoney,Group,People,SettingsOutlined,SettingsOverscan,Apartment,MicNone,Event,LocationDisabled,Assignment,ListAlt,AccountTree,Tune,
+    SupervisedUserCircle,TransferWithinAStation,SupervisorAccount
+} from '@material-ui/icons';
 
 
