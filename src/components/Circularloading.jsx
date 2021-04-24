302205e509a2527c9dacb70430fc1c293508a85b
FaizanAqeek <faizanaqeel10@gmail.com>
2021-04-24 23:08:21 +0500

    Merge branch 'main' of https://github.com/FaizanAqeek/HCMSAPP

--- src/components/Circularloading.jsx
+++ src/components/Circularloading.jsx
@@ -2,6 +2,33 @@ import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { Grid } from '@material-ui/core';
 
+
+{/* <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                zIndex: 10000,
+                backgroundColor: "#FFF",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                justifyContent: "center",
+                marginTop: "auto",
+                marginBottom: "auto",
+            }}
+        >
+            <CircularProgress
+                size="6rem"
+                style={{
+                    color: "#e8eaef",
+                    margin: 0,
+                    padding: 0,
+                }}
+            />
+        </div> */}
 function CircularLoading() {
 
   return (
