fe6f73ea8dbf000089ebd100e149fc6e3374ec15
786TOUSEEF <71210711+786TOUSEEF@users.noreply.github.com>
2022-02-24 13:22:52 -0800

    insert update delete area

--- src/pages/Organization/_Service.js
+++ src/pages/Organization/_Service.js
@@ -1,6 +1,14 @@
 export const API = {
-    GET_REGULAR_DROPDOWN:"common/getregulardropdown",
-    INSERT_COUNTRY:"orgnization/insertCountry",
-    UPDATE_COUNTRY:"orgnization/updateCountry"
+    GET_REGULAR_DROPDOWN: "common/getregulardropdown",
+
+    INSERT_COUNTRY: "orgnization/insertCountry",
+    UPDATE_COUNTRY: "orgnization/updateCountry",
+    GET_COUNTRY: "orgnization/getCountry",
+    DELETE_COUNTRY: "orgnization/deleteCountry",
+
+    INSERT_AREA: "orgnization/insertArea",
+    UPDATE_AREA: "orgnization/updateArea",
+    GET_AREA: "orgnization/getArea",
+    DELETE_AREA: "orgnization/getArea",
 }
 
--- src/pages/Organization/components/Area.js
+++ src/pages/Organization/components/Area.js
@@ -1,99 +1,98 @@
-import React, {  useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Controls from '../../../components/controls/Controls';
 import Popup from '../../../components/Popup';
-import {AutoForm} from '../../../components/useForm';
+import { AutoForm } from '../../../components/useForm';
 import { API } from '../_Service';
-import {useDispatch,useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { handlePostActions } from '../../../store/actions/httpactions';
 import useDropDownData from "../../../components/useDropDownData";
+import DataGrid from '../../../components/useDataGrid';
 
 export default function Area() {
-    const [openPopup, setOpenPopup] = useState(false);
-    const formRef = React.useRef(null);
-    
-    const dispatch = useDispatch();
-    const {countries,cities,states,setOption} = useDropDownData();
-    
+  const [openPopup, setOpenPopup] = useState(false);
+  const formRef = React.useRef(null);
 
-    const filterState = (data) => {
-        setOption({type:"country",data:[data],matchWith:"id"});
-      }
-    
-      const filterCity = (data) => {
-        setOption({type:"state",data:[data],matchWith:"id"});
-      }
+  const dispatch = useDispatch();
+  const { countries, cities, states, setOption } = useDropDownData();
 
-    const handleSubmit = (e) => {
-        const { getValue, validateFields } = formRef.current
-        const values = getValue();
-        if (validateFields()) {
-            dispatch(handlePostActions(API.INSERT_COUNTRY, values)).then(res => {
-                console.log(res);
-            });
-        }
+
+  const filterState = (data) => {
+    setOption({ type: "country", data: [data], matchWith: "id" });
+  }
+
+  const filterCity = (data) => {
+    setOption({ type: "state", data: [data], matchWith: "id" });
+  }
+
+  const handleSubmit = (e) => {
+    const { getValue, validateFields } = formRef.current
+    const values = getValue();
+    if (validateFields()) {
+      dispatch(handlePostActions(API.INSERT_AREA, values)).then(res => {
+        console.log(res);
+      });
     }
-    const formData = [
-        {
-            elementType: "ad_dropdown",
-            name: "fkCompanyId",
-            label: "Country",
-            required: true,
-            validate: {
-              errorMessage: "Company is required",
-            },
-            dataName: 'name',
-            options: countries,
-            onChange: filterState,
-            defaultValue: countries?.length ? countries[0] : null
-          },
-          {
-            elementType: "ad_dropdown",
-            name: "fkStateId",
-            label: "State",
-            required: true,
-            dataName: "name",
-            validate: {
-              errorMessage: "State is required",
-            },
-            options: states,
-            onChange: filterCity,
-            defaultValue: null
-          },
-          {
-            elementType: "ad_dropdown",
-            name: "fkCityId",
-            label: "City",
-            required: true,
-            dataName: "name",
-            validate: {
-              errorMessage: "City is required",
-            },
-            options: cities,
-            defaultValue: null
-          },
-          {
-            elementType: "inputfield",
-            name: "areaName",
-            label: "Area",
-            required: true,
-            validate: {
-              errorMessage: "Area is required"
-            },
-            defaultValue: ""
-          },
+  }
+  const formData = [
 
-    ];
-    return (
-        <>
-            <Popup
-                title="Add Country"
-                openPopup={openPopup}
-                maxWidth="sm"
-                addOrEditFunc={handleSubmit}
-                setOpenPopup={setOpenPopup}>
-                <AutoForm formData={formData} ref={formRef} isValidate={true} />
-            </Popup>
-            <Controls.Button onClick={() => { setOpenPopup(true) }} text="Add Area" />
-        </>
-    );
+    {
+      elementType: "inputfield",
+      name: "areaName",
+      label: "Area",
+      required: true,
+      validate: {
+        errorMessage: "Area is required"
+      },
+      defaultValue: ""
+    },
+    {
+      elementType: "ad_dropdown",
+      name: "fkCountryId",
+      label: "Country",
+      required: true,
+      validate: {
+        errorMessage: "Company is required",
+      },
+      dataName: 'name',
+      options: countries,
+      onChange: filterState,
+      defaultValue: countries?.length ? countries[0] : null
+    },
+    {
+      elementType: "ad_dropdown",
+      name: "fkStateId",
+      label: "State",
+      required: true,
+      dataName: "name",
+      validate: {
+        errorMessage: "State is required",
+      },
+      options: states,
+      onChange: filterCity,
+      defaultValue: null
+    },
+    {
+      elementType: "ad_dropdown",
+      name: "fkCityId",
+      label: "City",
+      required: true,
+      dataName: "name",
+      validate: {
+        errorMessage: "City is required",
+      },
+      options: cities,
+      defaultValue: null
+    }
+
+  ];
+  return (
+      <><Popup
+          title="Add Country"
+          openPopup={openPopup}
+          maxWidth="sm"
+          addOrEditFunc={handleSubmit}
+          setOpenPopup={setOpenPopup}>
+          <AutoForm formData={formData} ref={formRef} isValidate={true} />
+      </Popup><Controls.Button onClick={() => { setOpenPopup(true); } } text="Add Area" /><DataGrid /></>
+  );
 }
