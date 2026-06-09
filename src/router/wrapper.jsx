be24d40da6c23e23f16f793185bec7d52a0a3a75
faizanaqeek <faizanaqeel10@gmail.com>
2026-06-09 14:58:56 +0500

    check cookie

--- src/router/wrapper.jsx
+++ src/router/wrapper.jsx
@@ -5,7 +5,7 @@ import { Navigate, Outlet, useLocation } from 'react-router-dom';
 const RouterWrapper = () => {
   const location = useLocation();
   const isAuthenticated = document.cookie.includes("is_Auth=true");
-
+  console.log("Auth Status:", document.cookie);
   if (!isAuthenticated) return <Navigate to="/" state={{ from: location }} />;
 
   return <Outlet />;
